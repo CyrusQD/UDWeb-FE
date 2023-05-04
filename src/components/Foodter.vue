4a6ca0ab39aea9ca8f7ae008479e2f7a443b31e1
CyrusQD <tranquocdat53201@gmail.com>
2023-05-05 00:38:35 +0700

    FE project

---
+++ src/components/Foodter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <p>&copy; 2023 My Website</p>
+  </footer>
+</template>
+<script>
+export default {
+  name: "FoodterPage",
+};
+</script>
+<style>
+footer {
+  background-color: rgb(111,111,111);
+  color: #000000;
+  padding: 1rem;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+}
+</style>
